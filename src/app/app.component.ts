fa3e1e8a5897f9ec9c2f37a03320c584864c0bc5
jimderson28 <jim.chu28@gamil.com>
2024-06-02 17:45:05 +0800

    feat(product): 產品卡片樣式實作

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { ProductCardComponent } from './product-card/product-card.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [ProductCardComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'shopping-cart';
+}
