25c219ee8f4fba91397421fc6d0cefc12020f660
jimderson28 <jim.chu28@gamil.com>
2024-06-09 19:38:55 +0800

    feat(product): 變更作者資訊為文字陣列

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,28 +14,28 @@ export class AppComponent {
   products = [
       new Product({
       name : "A 產品",
-      authors : " 作者A、作者B、作者C",
+      authors : ['作者A','作者B','作者C'],
       company : "博碩文化",
       imgUrl : "https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img",
       price : 158000,
       }),
       new Product({
       name : "B 產品",
-      authors : " 作者A、作者B、作者C",
+      authors : ['作者A','作者B','作者C'],
       company : "博碩文化",
       imgUrl : "https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img",
       price : 158000,
       }),
       new Product({
         name : "C 產品",
-        authors : " 作者A、作者B、作者C",
+        authors : ['作者A','作者B','作者C'],
         company : "博碩文化",
         imgUrl : "https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img",
         price : 158000,
         }),
         new Product({
           name : "D 產品",
-          authors : " 作者A、作者B、作者C",
+          authors : ['作者A','作者B','作者C'],
           company : "博碩文化",
           imgUrl : "https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img",
           price : 158000,
