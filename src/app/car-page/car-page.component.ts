2bdf3a8eb1756cc605646a7794b58688fabfb3bf
jimderson28 <jim.chu28@gamil.com>
2024-06-09 20:12:59 +0800

    feat(car-page): 新增購物車頁面實做

---
+++ src/app/car-page/car-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-car-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './car-page.component.html',
+  styleUrl: './car-page.component.css'
+})
+export class CarPageComponent {
+
+}
