977304c4c2371ed80b4b8caab0c9f17210ad3ed5
jimderson28 <jim.chu28@gamil.com>
2024-06-09 18:11:02 +0800

    feat(product): 建立產品卡片清單元件

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,14 @@
 import { Component } from '@angular/core';
 import { ProductCardComponent } from './product-card/product-card.component';
+import { ProductCardListComponent } from './product-card-list/product-card-list.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [ProductCardComponent],
+  imports: [ProductCardListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  productName = "A 產品";
-  authors = " 作者A、作者B、作者C";
-  company = "博碩文化";
-  imgUrl = "https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img";
-  price = 158000;
+
 }
--- src/app/product-card/product-card.component.ts
+++ src/app/product-card/product-card.component.ts
@@ -1,5 +1,5 @@
 import { CurrencyPipe } from '@angular/common';
-import { Component, Input, numberAttribute } from '@angular/core';
+import { Component, HostBinding, Input, numberAttribute } from '@angular/core';
 
 @Component({
   selector: 'app-product-card',
@@ -14,4 +14,6 @@ export class ProductCardComponent {
   @Input() company!: string;
   @Input() imgUrl!: string ;
   @Input({ transform: numberAttribute }) price!: number;
+  @HostBinding('class')
+  class='product-card';
 }
