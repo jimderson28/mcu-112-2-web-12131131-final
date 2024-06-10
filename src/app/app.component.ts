c61a9a2816e0ba1e735f3b82af12cdfe27f4625a
jimderson28 <jim.chu28@gamil.com>
2024-06-10 15:37:47 +0800

    feat(router): 切換產品清單與登錄頁面實作

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { ProductCardComponent } from './product-card/product-card.component';
 import { ProductCardListComponent } from './product-card-list/product-card-list.component';
 import { Product } from './model/product';
 import { HeaderComponent } from './header/header.component';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [RouterOutlet,HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { ProductPageComponent } from './product-page/product-page.component';
+import { CarPageComponent } from './car-page/car-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'products' },
+  { path: 'products', component: ProductPageComponent },
+  { path: 'car', component: CarPageComponent },
+
+];
