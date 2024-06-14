25d6079702b734c6828a74a58ecf2bfd2dc31085
jimderson28 <jim.chu28@gamil.com>
2024-06-14 15:40:57 +0800

    feat(product-detail): 產品檢視頁面切版實作

--- src/app/product-detail-page/product-detail-page.component.ts
+++ src/app/product-detail-page/product-detail-page.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { Product } from '../model/product';
+import { CurrencyPipe } from '@angular/common';
 
 @Component({
   selector: 'app-product-detail-page',
   standalone: true,
-  imports: [],
+  imports: [CurrencyPipe],
   templateUrl: './product-detail-page.component.html',
   styleUrl: './product-detail-page.component.css'
 })
 export class ProductDetailPageComponent {
+product =
+new Product({
+name : "A 產品",
+authors : ['作者A','作者B','作者C'],
+company : "博碩文化",
+imgUrl : "https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img",
+price : 158000,
+})
+
+private router = inject (Router);
+
+onCars(): void{
+  this.router.navigate(['product','car']);
+}
+
+onBack(): void {
+  this.router.navigate(['products']);
+}
 
 }
